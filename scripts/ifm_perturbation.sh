4a7af3763e4d5f3f359055f68cfe5f1395abaedc
conradlee123 <conradclee123@gmail.com>
2024-06-06 17:04:27 -0400

    getting started with jobs

--- scripts/ifm_perturbation.sh
+++ scripts/ifm_perturbation.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ifm_pert               # Job name
 #SBATCH --output logs/ifm_pert_%J.log        # Output log file
 #SBATCH --mail-type=ALL                            # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=daniel.levine@yale.edu                       # Where to send mail
+#SBATCH --mail-user=conrad.lee@yale.edu                       # Where to send mail
 #SBATCH --partition gpu
 #SBATCH --requeue
 #SBATCH --nodes=1	
@@ -15,23 +15,33 @@ date;hostname;pwd
 
 module load miniconda
 module load CUDA/12.1
-conda activate c2s2
-cd /home/dfl32/project/ifm
+conda activate ifm
+cd /gpfs/gibbs/project/dijk/ccl66/ifm
 export TOKENIZERS_PARALLELISM=true
+
 
 python ifm_perturbation.py \
     --model_name EleutherAI/pythia-160m \
-    --llm_dataset_path /home/dfl32/scratch/cinemaot_data/ifm_hf_ds/gaussian_768_hf_ds \
-    --num_train_epochs 100 \
+    --llm_dataset_path /home/dfl32/palmer_scratch/ifm/cinemaot_data/ifm_hf_ds/gaussian_768_hf_ds \
     --train_gaussian True \
-    --time_points 100 \
+    --num_train_epochs 100 \
+    --time_points 16 \
     --max_context_length 50 \
     --max_num_blocks 32 \
-    --per_device_train_batch_size 256 \
-    --per_device_eval_batch_size 256 \
+    --train_dataset_size 5000 \
+    --per_device_train_batch_size 128 \
+    --per_device_eval_batch_size 128 \
     --eval_accumulation_steps 5 \
     --gradient_accumulation_steps 1 \
     --save_steps 1000 \
     --e2e True \
+    --train_custom True \
+    --hdim_2d 64 \
+    --idim_2d 64 \
+    --nheads_2d 4 \
+    --nblocks_2d 2 \
+    --straight_paths True \
     --use_vae True \
     --kl_weight 0.3 \
+    --scale_last True \
+    --output_dir /home/ccl66/palmer_scratch/ifm/training-runs
